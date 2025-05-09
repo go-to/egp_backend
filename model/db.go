3d6a78aa1ff6473f8ae8fe140ee3c5b6d7aeaa41
go-to <go8ogle@gmail.com>
2025-05-09 09:47:08 +0900

    GORMのクエリログを出力しないように修正

--- model/db.go
+++ model/db.go
@@ -3,6 +3,7 @@ package model
 import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 type DB struct {
@@ -10,7 +11,9 @@ type DB struct {
 }
 
 func Init(dsn string) (DB, error) {
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
 	if err != nil {
 		panic(err)
 	}
