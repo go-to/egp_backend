5492e45bd4872aa3f550407d2cab71793d6e0c51
go-to <go8ogle@gmail.com>
2025-06-20 17:12:09 +0900

    初回起動時に実行するSQLを統合

--- docker/postgres/init/init.sql
+++ docker/postgres/init/init.sql
@@ -1,3 +1,9 @@
+-- DB作成
+CREATE DATABASE IF NOT EXISTS egp;
+
 -- スキーマ作成
 CREATE SCHEMA IF NOT EXISTS egp;
 COMMENT ON SCHEMA egp IS 'ヱビスビールに合う逸品グランプリ';
+
+-- postgis有効化
+CREATE EXTENSION postgis WITH SCHEMA egp;
